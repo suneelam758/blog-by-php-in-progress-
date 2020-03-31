1e195899b11060e02b352dfdf4770377aa588afc
suneelam758 <suneelamtripathi123456@gmail.com>
2020-03-31 16:07:09 +0530

    first commit

---
+++ includes/navbar.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="navbar">
+	<div class="logo_div">
+		<a href="index.php"><h1>LifeBlog</h1></a>
+	</div>
+	<ul>
+	  <li><a class="active" href="index.php">Home</a></li>
+	  <li><a href="#news">News</a></li>
+	  <li><a href="#contact">Contact</a></li>
+	  <li><a href="#about">About</a></li>
+	</ul>
+</div>
+</body>
+</html>
